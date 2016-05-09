b7a66b5377bf6c8a29fe36a2e50becd2982db932
kristynlyncheski <klyncheski@gmail.com>
2016-05-08 22:11:17 -0400

    deploying frontend

--- app/utils/ajaxHelpers.js
+++ app/utils/ajaxHelpers.js
@@ -2,35 +2,35 @@ import axios from 'axios';
 
 const helpers = {
   addUser: function(user){
-    return axios.post('http://localhost:3000/users.json', user);
+    return axios.post('https://agile-tundra-88608.herokuapp.com/users.json', user);
   },
 
   updateUser: function(user){
-    return axios.put('http://localhost:3000/users/' + user.user_id + '.json', user);
+    return axios.put('https://agile-tundra-88608.herokuapp.com/users/' + user.user_id + '.json', user);
   },
 
   getUserInfo: function(user){
-    return axios.get('http://localhost:3000/users/' + user.user_id + '.json', user);
+    return axios.get('https://agile-tundra-88608.herokuapp.com/users/' + user.user_id + '.json', user);
   },
 
   addSong: function(user){
-    return axios.post('http://localhost:3000/songs.json', user);
+    return axios.post('https://agile-tundra-88608.herokuapp.com/songs.json', user);
   },
 
   addUserSong: function(selectionInfo){
-    return axios.post('http://localhost:3000/songs_users.json', selectionInfo);
+    return axios.post('https://agile-tundra-88608.herokuapp.com/songs_users.json', selectionInfo);
   },
 
   updateUserSong: function(data){
-    return axios.put('http://localhost:3000/songs_users/added/' + data.user_id + '/' + data.song_id + ".json", data);
+    return axios.put('https://agile-tundra-88608.herokuapp.com/songs_users/added/' + data.user_id + '/' + data.song_id + ".json", data);
   },
 
   getSavedSongs: function(user){
-    return axios.get('http://localhost:3000/songs/saved/' + user.user_id + '.json');
+    return axios.get('https://agile-tundra-88608.herokuapp.com/songs/saved/' + user.user_id + '.json');
   },
 
   getHistory: function(user){
-    return axios.get('http://localhost:3000/songs_users/history/' + user.user_id + '.json');
+    return axios.get('https://agile-tundra-88608.herokuapp.com/songs_users/history/' + user.user_id + '.json');
   },
 
 
