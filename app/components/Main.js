dea23ff28485c13dd5405914832686907cfb9065
kristynlyncheski <klyncheski@gmail.com>
2016-05-09 09:33:28 -0400

    bkg img

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,12 +2,12 @@ import React from 'react';
 var styles = require('../styles/Styles.css');
 import Header from '../containers/Header';
 import Footer from '../containers/Footer';
-// var bg = {
-//   backgroundImage: "url('app/assets/background-img-overlay.jpg')",
-//   backgroundRepeat: "no-repeat",
-//   backgroundSize: "cover",
-//   backgroundPosition:"bottom"
-// }
+var bg = {
+  backgroundImage: "url('app/assets/background-img-overlay.jpg')",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition:"bottom"
+}
 
 const Main = React.createClass({
   getInitialState: function(){
